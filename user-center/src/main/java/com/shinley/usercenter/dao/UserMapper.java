763dfb6ec48b0b1f4433e1df89fa568fa9df5588
shinley <chenxl4@autoai.com>
2020-11-09 17:18:07 +0800

    first commit

---
+++ user-center/src/main/java/com/shinley/usercenter/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.shinley.usercenter.dao;
+
+import com.shinley.usercenter.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
---
+++ user-center/src/main/java/com/shinley/usercenter/entity/User.java
@@ -0,0 +1,57 @@
+package com.shinley.usercenter.entity;
+
+import java.util.Date;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    /**
+     * 微信id
+     */
+    @Column(name = "wx_id")
+    private String wxId;
+
+    /**
+     * 微信昵称
+     */
+    @Column(name = "wx_nickname")
+    private String wxNickname;
+
+    /**
+     * 角色
+     */
+    private String roles;
+
+    /**
+     * 头像
+     */
+    @Column(name = "avatr_url")
+    private String avatrUrl;
+
+    /**
+     * 积分
+     */
+    private Integer bonus;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @Column(name = "update_time")
+    private Date updateTime;
+}
