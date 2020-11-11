6bbddcd5e866e49a848c8def0be604573a83be04
shinley <chenxl4@autoai.com>
2020-11-11 13:48:48 +0800

    整合feign

--- content-center/src/main/java/com/shinley/contentcentter/service/ShareService.java
+++ content-center/src/main/java/com/shinley/contentcentter/service/ShareService.java
@@ -4,6 +4,7 @@ import com.shinley.contentcentter.dao.ShareMapper;
 import com.shinley.contentcentter.dto.ShareDto;
 import com.shinley.contentcentter.dto.UserDto;
 import com.shinley.contentcentter.entity.Share;
+import com.shinley.contentcentter.feignapi.UserCenterFeignClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +19,21 @@ public class ShareService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UserCenterFeignClient userCenterFeignClient;
+
     public ShareDto findById(Integer id) {
         // 获取分享详情
         Share share = this.shareMapper.selectByPrimaryKey(id);
         // 发布人id
         Integer userId = share.getUserId();
 
-        UserDto userDto = restTemplate.getForObject(
-                "http://localhost:8081/users/{id}",
-                UserDto.class,
-                userId
-        );
+//        UserDto userDto = restTemplate.getForObject(
+//                "http://localhost:8081/users/{id}",
+//                UserDto.class,
+//                userId
+//        );
+        UserDto userDto = userCenterFeignClient.findById(userId);
         // 消息的装配
         ShareDto shareDto = new ShareDto();
         BeanUtils.copyProperties(share, shareDto);
